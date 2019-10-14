1700c5ade3fb4d48c9782deaebcc9d2b907a988d
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 17:13:15 -0400

    Updates

---
+++ precache-manifest.71fdde177296380363180470371eeacb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd43805057c596b92fb6fcef47c9ac95",
+    "url": "/woks/index.html"
+  },
+  {
+    "revision": "d184825d9e5c3cc4a07f",
+    "url": "/woks/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/woks/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "d184825d9e5c3cc4a07f",
+    "url": "/woks/static/js/main.4ecbc89d.chunk.js"
+  },
+  {
+    "revision": "37b1a2b3978ac20e1d7b",
+    "url": "/woks/static/js/runtime~main.bd19b5d1.js"
+  }
+]);
